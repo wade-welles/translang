a0a60010442534820d5c956305c44897d0bf5375
Your Name <you@example.com>
2019-10-25 21:05:09 -0400

    experimenting with the slice object we are creating

--- experiment.rb
+++ experiment.rb
@@ -3,13 +3,41 @@ require 'ffi'
 
 module Go
   extend FFI::Library
-  # define class GoSlice to map to:
-  # C type struct { void *data; GoInt len; GoInt cap; }
   class Slice < FFI::Struct
     layout :data,  :pointer,
            :len,   :long_long,
            :cap,   :long_long 
 
+    def self.value 
+      return self.val
+    end
+
+    def initialize(slice)
+      self[:data] = FFI::MemoryPointer.new(:long_long, slice.size)
+      self[:len] = slice.size
+      self[:cap] = slice.size 
+      return self
+    end
+  end
+
+  # define class GoSlice to map to:
+  # C type struct { void *data; GoInt len; GoInt cap; }
+  class IntSlice < FFI::Struct
+    layout :data,  :pointer,
+           :len,   :long_long,
+           :cap,   :long_long 
+
+    def self.value 
+      return self.val
+    end
+
+    def initialize(slice)
+      self[:data] = FFI::MemoryPointer.new(:long_long, slice.size)
+      self[:data].write_array_of_long_long(slice)
+      self[:len] = slice.size
+      self[:cap] = slice.size 
+      return self
+    end
   end
 
   # define class GoString to map:
@@ -33,8 +61,10 @@ module Go
     extend FFI::Library
     ffi_lib './go.so'
     attach_function :greeter, [String.value], :void
-    attach_function :is_blank, [:string], :bool
+    attach_function :int_slice_size, [IntSlice.value], :int
   end
 end
 
 Go::Functions.greeter(Go::String.new("test"))
+
+p "size: #{Go::Functions.int_slice_size(Go::IntSlice.new([5, 3, 2]))}"
--- main.go
+++ main.go
@@ -7,14 +7,10 @@ import (
 )
 
 //export greeter
-func greeter(cstr string) {
-	fmt.Printf("hello %v\n", cstr)
-}
+func greeter(cstr string) { fmt.Printf("hello %v\n", cstr) }
 
 //export is_blank
-func is_blank(str string) bool {
-	return len(str) == 0
-}
+func is_blank(str string) bool { return len(str) == 0 }
 
 //export adder
 func adder(a, b int) (sum int) {
@@ -22,5 +18,8 @@ func adder(a, b int) (sum int) {
 	fmt.Printf("sum: %v\n", sum)
 	return sum
 }
+
+//export int_slice_size
+func int_slice_size(slice []int) int { return len(slice) }
 
 func main() {}
